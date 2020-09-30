dcbb7898c56731aa79521b18e0c04358816530fe
dzuongld <5718860+dzuongld@users.noreply.github.com>
2020-09-30 10:58:06 +1000

    Bug fixes

--- blog_clone/urls.py
+++ blog_clone/urls.py
@@ -20,6 +20,6 @@ from django.contrib.auth import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('blog.urls')),
-    path('accounts/login/',views.auth_login, name='login'),
-    path('accounts/logout/',views.auth_logout, name='logout', kwargs={'next_page': '/'}),
+    path('accounts/login/',views.LoginView.as_view(), name='login'),
+    path('accounts/logout/',views.LogoutView.as_view(next_page='/'), name='logout'),
 ] # yapf:disable
